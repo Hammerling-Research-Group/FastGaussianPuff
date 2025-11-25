fix annoying off by one error in puff code where last step would have messed up averaging

tests/test_regression.py
@@ -28,6 +28,7 @@ def test_regression():
         test = pd.read_csv(test_name)
 
         for name in cols:
-            assert np.allclose(ref[name].values, test[name].values, atol=tol)
+            # old regression files have one extra row
+            assert np.allclose(ref[name].values[:-1], test[name].values, atol=tol)
 
     assert num_comp == num_refs